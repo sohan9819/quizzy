1063e0c18d05de6c81cdf35aca5f5a9e107692da
Sohan <sohanshetty2001@gmail.com>
2022-07-15 15:54:56 +0530

    removed unwanted

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
@@ -13,9 +11,7 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
